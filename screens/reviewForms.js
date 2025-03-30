059b83a6a32aacad94538349c3a38fb26d92aa3f
Nigel <nigel.mahlako@investec.com>
2025-03-30 22:48:31 +0200

    Added display of error validations

--- screens/reviewForms.js
+++ screens/reviewForms.js
@@ -2,16 +2,19 @@ import React from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 import { globalStyles } from "../styles/global";
 import { Formik } from "formik";
-import * as yup from 'yup';
+import * as yup from "yup";
 
 const reviewSchema = yup.object({
   title: yup.string().required().min(4),
   body: yup.string().required().min(8),
-  rating: yup.string().required().test('isNum1-5', 'Rating mubst be 1-5', (val) => {
-    return parseInt(val) < 6 && parseInt(val)>0;
-  })
-
-})
+  rating: yup
+    .string()
+    .required()
+    .test("isNum1-5", "Rating mubst be 1-5", (val) => {
+      console.log(">>>>>> " + parseInt(val));
+      return parseInt(val) <= 5 && parseInt(val) >= 1;
+    }),
+});
 
 export default function ReviewForm({ addReview }) {
   return (
@@ -24,9 +27,9 @@ export default function ReviewForm({ addReview }) {
         }}
         validationSchema={reviewSchema}
         onSubmit={(values, actions) => {
-         actions.resetForm(); //Automatically crears the form
-         addReview(values);
-         // console.log(values)
+          actions.resetForm(); //Automatically crears the form
+          addReview(values);
+          // console.log(values)
         }}
       >
         {(props) => (
@@ -36,7 +39,11 @@ export default function ReviewForm({ addReview }) {
               placeholder="Review Title"
               onChangeText={props.handleChange("title")}
               value={props.values.title}
+              onBlur={props.handleBlur("title")}
             />
+            <Text style={Styles.errorText}>
+              {props.touched.title && props.errors.title}
+            </Text>
 
             <TextInput
               multiline
@@ -44,7 +51,11 @@ export default function ReviewForm({ addReview }) {
               placeholder="Review body"
               onChangeText={props.handleChange("body")}
               value={props.values.body}
+              onBlur={props.handleBlur("body")}//Real time feedback on errors
             />
+            <Text style={Styles.errorText}>
+              {props.touched.body && props.errors.body} 
+            </Text>
 
             <TextInput
               style={globalStyles.input}
@@ -52,7 +63,12 @@ export default function ReviewForm({ addReview }) {
               onChangeText={props.handleChange("rating")}
               value={props.values.rating}
               keyboardType="numeric"
+              onBlur={props.handleBlur("rating")}
             />
+            <Text style={Styles.errorText}>
+              {props.touched.rating && props.errors.rating}
+            </Text>
+
             <Button title="SUBMIT" color="red" onPress={props.handleSubmit} />
           </View>
         )}
@@ -60,3 +76,12 @@ export default function ReviewForm({ addReview }) {
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  errorText: {
+    color: "red",
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 3,  
+  },
+});
