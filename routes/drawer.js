f0431f6c485bbb250fce70803c0a5fb538453fe3
Nigel <nigel.mahlako@investec.com>
2025-03-30 16:59:28 +0200

    feat(Add model feature): This will open a modal and has ability to close it as well
    Created a modal will open and close icons,tested on both IOS and Android
    
    BREAKING CHANGE:

--- routes/drawer.js
+++ routes/drawer.js
@@ -24,7 +24,7 @@ export default function App() {
         }}
       >
         <Drawer.Screen 
-          name="GamingTest" 
+          name="Home" 
           component={HomeStack}
           options={{
             drawerIcon: ({ color }) => (
--- screens/Home.js
+++ screens/Home.js
@@ -26,9 +26,11 @@ import {
   Text,
   FlatList,
   TouchableOpacity,
+  Modal,
 } from "react-native";
 import { globalStyles } from "../styles/global";
 import Card from "./shared/card";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function Home({ navigation }) {
   const [reviews, setReviews] = useState([
@@ -40,8 +42,39 @@ export default function Home({ navigation }) {
     { title: "number6", rating: 3, body: "lorem ipsum", key: "5" },
   ]);
 
+  const [modalDisplay, setModalDisplay] = useState(false);
+
+  function setModalOpen(value) {
+    setModalDisplay(value);
+  }
+
   return (
     <View style={globalStyles.container}>
+
+      <Modal visible={modalDisplay} animationType="slide">
+        <View>
+        <MaterialIcons
+          name="close"
+          style={{...Styles.modalToggle,...Styles.modalClose}}
+          size={24}
+          onPress={() => {
+            setModalOpen(false);
+          }}
+        />
+        <Text style={Styles.modal}>Are you old enough to play?</Text>
+        </View>
+      
+      </Modal>
+
+      <MaterialIcons
+        name="add"
+        size={24}
+        style={Styles.modalToggle}
+        onPress={() => {
+          setModalOpen(true);
+        }}
+      />
+
       <FlatList
         data={reviews}
         renderItem={({ item }) => (
@@ -57,3 +90,23 @@ export default function Home({ navigation }) {
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  modal: {
+    padding: 20,
+    margin: 10,
+    backgroundColor: "yellow",
+  },
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "red",
+    padding: 10,
+    borderRadius: 10,
+    alignSelf:'center',
+  },
+  modalClose: {
+    marginTop: 40,
+    marginBottom:10,
+  }
+});
