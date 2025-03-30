ca16612d997d90feb8b62582c119e6b1475ee72d
Nigel <nigel.mahlako@investec.com>
2025-03-30 19:47:22 +0200

    test markdown

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ To install the drawer navigation, run the following command:
 
 ```bash
 npm install @react-navigation/drawer
+
+To Work with forms
+
+```bash
+npm i formik
+
+[link](https://formik.org/docs/guides/react-native)
---
+++ screens/reviewForms.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { globalStyles } from "../styles/global";
+import { Formik } from "formik";
+
+export default function ReviewForn() {
+  
+}
